6c90d7836dff84589e4bc90a658324edb5cd1ee9
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-21 22:39:09 +0800

    Create dictionary-comprehension-for-loops.py

---
+++ dictionary-comprehension-for-loops.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4]
+
+# creating an empty dictionary
+square_numbers = {}
+
+# using a loop to add items to dictionary
+for number in numbers:
+    square_numbers[number] = number**2
+
+print(square_numbers)
