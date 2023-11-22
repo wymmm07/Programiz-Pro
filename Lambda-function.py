ba7f055db551fbb793177e265285f3ae9a06a621
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-22 13:09:43 +0800

    Create Lambda-function.py
    Problem Description
    Create a lambda function that returns the square of a number passed as an argument.
    
    Create the lambda function and assign it to a variable named square.
    Take a float input from the user and assign it to the number variable.
    Call the lambda function with number as an argument and print the result.

---
+++ Lambda-function.py
@@ -0,0 +1,10 @@
+# create the function
+square = lambda n: n*n
+
+# take user input
+number = float(input("Enter number: "))
+
+# call the function and print the result
+result = square(number)
+
+print(result)
