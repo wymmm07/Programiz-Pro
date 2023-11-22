a5cb030bd7ebe4c1a222710ac351679017dc68fc
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-22 13:19:05 +0800

    Create lambda-function2.py
    Problem Description
    Create a lambda function that takes two arguments and returns their sum.
    
    Create the lambda function and assign it to a variable named add.
    Take two integer inputs and assign them to n1 and n2 respectively.
    Call the lambda function with n1 and n2 as arguments and print the result.

---
+++ lambda-function2.py
@@ -0,0 +1,10 @@
+# create the function
+add = lambda n1, n2: n1 + n2
+
+# take two integer inputs
+n1 = int(input("Enter n1: "))
+n2 = int(input("Enter n2: "))
+
+# call the function and print the result
+result = add(n1, n2)
+print(result)
